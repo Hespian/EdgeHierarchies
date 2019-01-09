e91a0234a7e967b55ca10daeae8ef04cff7dc2bd
Demian Hespe <hespe@kit.edu>
2019-01-09 10:13:30 +0100

    Add ArraySet implementation

---
+++ lib/arraySet.h
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * lib/arraySet.h
+ *
+ * Copyright (C) 2018 Demian Hespe <hespe@kit.edu>
+ *
+ * All rights reserved.
+ ******************************************************************************/
+
+#pragma once
+
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+template <typename T>
+class ArraySet {
+public:
+    ArraySet(T size) {
+        lookup.resize(size, std::numeric_limits<T>::max());
+        elements.resize(size);
+        endPtr = 0;
+    }
+
+    bool contains(T element) {
+        assert(element < lookup.size());
+        return lookup[element] < endPtr;
+    }
+
+    void insert(T element) {
+        assert(element < lookup.size());
+        assert(!contains(element));
+        lookup[element] = endPtr;
+        elements[endPtr++] = element;
+    }
+
+    void remove(T element) {
+        assert(element < lookup.size());
+        assert(contains(element));
+        elements[lookup[element]] = elements[--endPtr];
+        lookup[elements[endPtr]] = lookup[element];
+        lookup[element] = std::numeric_limits<T>::max();
+    }
+
+    T size() {
+        return endPtr;
+    }
+
+    T capacity() {
+        return lookup.size();
+    }
+
+    void resize(T newSize) {
+        assert(endPtr < newSize);
+        lookup.resize(newSize);
+        elements.resize(newSize);
+    }
+
+    typename vector<T>::iterator begin() { return elements.begin();   }
+    typename vector<T>::iterator end()   { return elements.begin() + endPtr; }
+
+    typename vector<T>::const_iterator begin() const { return elements.begin();   }
+    typename vector<T>::const_iterator end()   const { return elements.begin() + endPtr; }
+
+protected:
+    vector<T> lookup;
+    vector<T> elements;
+    T endPtr;
+
+};
---
+++ tests/arraySetTests.cpp
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * tests/arraySetTests.cpp
+ *
+ * Copyright (C) 2018 Demian Hespe <hespe@kit.edu>
+ *
+ * All rights reserved.
+ ******************************************************************************/
+
+#include <vector>
+#include <algorithm>
+
+#include <gtest/gtest.h>
+
+#include "arraySet.h"
+
+TEST(ArraySetTest, SimpleTest) {
+    ArraySet<unsigned> as(10);
+    EXPECT_EQ(as.capacity(), 10);
+    EXPECT_EQ(as.size(), 0);
+
+    as.resize(20);
+    EXPECT_EQ(as.capacity(), 20);
+    EXPECT_EQ(as.size(), 0);
+
+    EXPECT_FALSE(as.contains(5));
+    as.insert(5);
+    EXPECT_TRUE(as.contains(5));
+
+    EXPECT_FALSE(as.contains(1));
+    as.insert(1);
+    EXPECT_TRUE(as.contains(5));
+    EXPECT_TRUE(as.contains(1));
+
+    as.remove(5);
+    EXPECT_FALSE(as.contains(5));
+    EXPECT_TRUE(as.contains(1));
+
+    as.insert(3);
+    EXPECT_FALSE(as.contains(5));
+    EXPECT_TRUE(as.contains(1));
+    EXPECT_TRUE(as.contains(3));
+
+    std::vector<unsigned> elements;
+    for(int element : as) {
+        elements.push_back(element);
+    }
+
+    std::sort(elements.begin(), elements.end());
+
+    ASSERT_EQ(elements.size(), 2);
+    EXPECT_EQ(elements[0], 1);
+    EXPECT_EQ(elements[1], 3);
+}
