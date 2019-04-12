17fbedff543373976f44c98ea8ae2db6ca42dd11
Demian Hespe <hespe@kit.edu>
2019-04-12 11:26:32 +0200

    Add two more edge rankers

---
+++ lib/edgeRanking/levelShortcutsHopsEdgeRanker.h
@@ -0,0 +1,192 @@
+/*******************************************************************************
+ * lib/edgeRanking/levelShortcutsHopsEdgeRanker.h
+ *
+ * Copyright (C) 2019 Demian Hespe <hespe@kit.edu>
+ *
+ * All rights reserved.
+ ******************************************************************************/
+
+#pragma once
+
+#include "assert.h"
+#include <vector>
+
+#include "routingkit/id_queue.h"
+
+#include "definitions.h"
+#include "edgeIdCreator.h"
+#include "edgeHierarchyGraph.h"
+#include "bipartiteMinimumVertexCover.h"
+#include "shortcutHelper.h"
+
+
+class LevelShortcutsHopsEdgeRanker {
+
+public:
+    LevelShortcutsHopsEdgeRanker(EdgeHierarchyGraph &g) : g(g), PQ(g.getNumberOfEdges() * 2), numHops(g.getNumberOfEdges() * 2), level(g.getNumberOfEdges() * 2), mvc(g.getNumberOfNodes()), lastEdgeReturned(EDGEID_EMPTY_KEY), query(g), poppedCounter(0) {
+        std::cout << "Level shortcuts edge lazy ranker" <<std::endl;
+        g.forAllNodes( [&] (NODE_T u) {
+                g.forAllNeighborsOut(u, [&] (NODE_T v, EDGEWEIGHT_T weight) {
+                        addEdgeFresh(u, v);
+                    });
+            });
+    }
+
+
+    void addEdge(NODE_T u, NODE_T v) {
+        EDGEID_T edgeId = edgeIdCreator.getEdgeId(u, v);
+        if(edgeId > PQ.id_count()) {
+            increaseCapacity(2 * edgeId);
+        }
+
+        // numHops[edgeId] = 1;
+        unsigned maxLevel = 1;
+        g.forAllNeighborsOutWithHighRank(v, g.getEdgeRank(u, v), [&] (NODE_T x, EDGEWEIGHT_T weight, EDGERANK_T rank) {
+                EDGEID_T neighborEdgeId = edgeIdCreator.getEdgeId(v, x);
+                if(level[neighborEdgeId] > maxLevel) {
+                    maxLevel = level[neighborEdgeId];
+                }
+                // updateImportance(v, x);
+            });
+        g.forAllNeighborsInWithHighRank(u, g.getEdgeRank(u, v), [&] (NODE_T x, EDGEWEIGHT_T weight, EDGERANK_T rank) {
+                EDGEID_T neighborEdgeId = edgeIdCreator.getEdgeId(x, u);
+                if(level[neighborEdgeId] > maxLevel) {
+                    maxLevel = level[neighborEdgeId];
+                }
+                // updateImportance(x, u);
+            });
+        level[edgeId] = maxLevel + 1;
+
+        updateImportance(u, v);
+    }
+
+    void updateEdge(NODE_T u, NODE_T v) {
+        // g.forAllNeighborsOutWithHighRank(v, g.getEdgeRank(u, v), [&] (NODE_T x, EDGEWEIGHT_T weight, EDGERANK_T rank) {
+        //         updateImportance(v, x);
+        //     });
+        // g.forAllNeighborsInWithHighRank(u, g.getEdgeRank(u, v), [&] (NODE_T x, EDGEWEIGHT_T weight, EDGERANK_T rank) {
+        //         updateImportance(x, u);
+        //     });
+
+        // updateImportance(u, v);
+    }
+
+    pair<NODE_T, NODE_T> getNextEdge() {
+        // updateNeighborEdges();
+        auto popped = PQ.pop();
+        EDGEID_T edgeId = popped.id;
+        EDGEID_T edgeIdOld = EDGEID_EMPTY_KEY;
+        pair<NODE_T, NODE_T> edge = edgeIdCreator.getEdgeFromId(edgeId);
+        while(edgeId != edgeIdOld){
+            edgeIdOld = edgeId;
+            edge = edgeIdCreator.getEdgeFromId(edgeId);
+            auto newImportance = getEdgeImportance(edge.first, edge.second);
+
+            if(PQ.peek().key < newImportance) {
+                // cout << "LARGER!" << endl;
+                PQ.push({edgeId, newImportance});
+                popped = PQ.pop();
+                edgeId = popped.id;
+            }
+        }
+        lastEdgeReturned = edgeId;
+        // poppedCounter++;
+        // if(poppedCounter % 1000 == 0) {
+        //     std::cout << poppedCounter << " out of " << PQ.size() << std::endl;
+        // }
+        if(g.getEdgeRank(edge.first, edge.second) != EDGERANK_INFINIY) {
+            std::cout << "returned invalid edge!" << std::endl;
+        }
+        return edge;
+    }
+
+    bool hasNextEdge() {
+        return !PQ.empty();
+    }
+protected:
+
+    void updateNeighborEdges() {
+
+        if(lastEdgeReturned == EDGEID_EMPTY_KEY) {
+            return;
+        }
+        pair<NODE_T, NODE_T> edge = edgeIdCreator.getEdgeFromId(lastEdgeReturned);
+        NODE_T u = edge.first;
+        NODE_T v = edge.second;
+
+        unsigned newLevel = level[lastEdgeReturned] + 1;
+
+        g.forAllNeighborsOutWithHighRank(v, g.getEdgeRank(u, v), [&] (NODE_T x, EDGEWEIGHT_T weight, EDGERANK_T rank) {
+                EDGEID_T neighborEdgeId = edgeIdCreator.getEdgeId(v, x);
+                if(level[neighborEdgeId] < newLevel) {
+                    level[neighborEdgeId] = newLevel;
+                }
+                updateImportance(v, x);
+            });
+        g.forAllNeighborsInWithHighRank(u, g.getEdgeRank(u, v), [&] (NODE_T x, EDGEWEIGHT_T weight, EDGERANK_T rank) {
+                EDGEID_T neighborEdgeId = edgeIdCreator.getEdgeId(x, u);
+                if(level[neighborEdgeId] < newLevel) {
+                    level[neighborEdgeId] = newLevel;
+                }
+                updateImportance(x, u);
+            });
+    }
+
+    unsigned getEdgeImportance(NODE_T u, NODE_T v) {
+        if(g.getEdgeRank(u, v) != EDGERANK_INFINIY) {
+            std::cout << "Updating importance of removed edge!" <<std::endl;
+        }
+        EDGEID_T edgeId = edgeIdCreator.getEdgeId(u, v);
+        g.setEdgeRank(u, v, EDGERANK_INFINIY - 1);
+        auto shortestPathsLost = getShortestPathsLost<false>(u, v, g.getEdgeWeight(u, v), g, query);
+        g.setEdgeRank(u, v, EDGERANK_INFINIY);
+        auto numShortcutEdges = mvc.getMinimumVertexCoverSize(shortestPathsLost.first);
+
+        return 1 + 1000*level[edgeId] + (1000*numShortcutEdges);
+    }
+
+    void updateImportance(NODE_T u, NODE_T v) {
+        EDGEID_T edgeId = edgeIdCreator.getEdgeId(u, v);
+
+        auto importance = getEdgeImportance(u, v);
+
+
+        if(!PQ.contains_id(edgeId)) {
+            PQ.push({edgeId, importance});
+        } else {
+            unsigned currentKey = PQ.get_key(edgeId);
+            if(importance < currentKey) {
+                PQ.decrease_key({edgeId, importance});
+            }
+            if(importance > currentKey) {
+                PQ.increase_key({edgeId, importance});
+            }
+        }
+    }
+
+    void addEdgeFresh(NODE_T u, NODE_T v) {
+        EDGEID_T edgeId = edgeIdCreator.getEdgeId(u, v);
+        numHops[edgeId] = 1;
+        level[edgeId] = 1;
+
+        updateImportance(u, v);
+    }
+
+    void increaseCapacity(size_t size) {
+		PQ.id_pos.resize(size, RoutingKit::invalid_id);
+        PQ.heap.resize(size);
+        numHops.resize(size);
+        level.resize(size);
+    }
+
+    EdgeHierarchyGraph &g;
+    EdgeIdCreator edgeIdCreator;
+    RoutingKit::MinIDQueue PQ;
+    std::vector<unsigned> numHops;
+    std::vector<unsigned> level;
+    BipartiteMinimumVertexCover mvc;
+    EDGEID_T lastEdgeReturned;
+    EdgeHierarchyQuery query;
+    unsigned poppedCounter = 0;
+
+};
---
+++ lib/edgeRanking/shortcutCountingSortingRoundsEdgeRanker.h
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * lib/edgeRanking/shortcutCountingSortingRoundsEdgeRanker.h
+ *
+ * Copyright (C) 2018-2019 Demian Hespe <hespe@kit.edu>
+ *
+ * All rights reserved.
+ ******************************************************************************/
+
+#pragma once
+
+#include <vector>
+#include <utility>
+#include "assert.h"
+
+#include "definitions.h"
+#include "edgeIdCreator.h"
+#include "edgeHierarchyGraph.h"
+#include "edgeHierarchyQuery.h"
+#include "bipartiteMinimumVertexCover.h"
+#include "arraySet.h"
+#include "shortcutHelper.h"
+
+using namespace std;
+
+class ShortcutCountingSortingRoundsEdgeRanker {
+
+public:
+    ShortcutCountingSortingRoundsEdgeRanker(EdgeHierarchyGraph &g) : g(g), query(g), mvc(g.getNumberOfNodes()), numShortcutEdges(g.getNumberOfEdges()), edgesInGraph(g.getNumberOfEdges()) {
+        std::cout << "Shortcut counting sorting rounds edge ranker" << std::endl;
+        g.forAllNodes( [&] (NODE_T u) {
+                g.forAllNeighborsOut(u, [&] (NODE_T v, EDGEWEIGHT_T weight) {
+                        addEdge(u, v);
+                    });
+            });
+    }
+
+    void addEdge(NODE_T u, NODE_T v) {
+        EDGEID_T edgeId = edgeIdCreator.getEdgeId(u, v);
+        if(edgesInGraph.capacity() <= edgeId) {
+            edgesInGraph.resize(edgesInGraph.capacity() * 2);
+            numShortcutEdges.resize(numShortcutEdges.size() * 2);
+            assert(numShortcutEdges.size() == edgesInGraph.capacity());
+        }
+        edgesInGraph.insert(edgeId);
+    }
+
+    void updateEdge(NODE_T u, NODE_T v) {
+    }
+
+    pair<NODE_T, NODE_T> getNextEdge() {
+        if(currentRoundEdges.size() == 0) {
+            getNextRoundEdges();
+        }
+
+        EDGEID_T nextEdgeId = currentRoundEdges.back();
+        currentRoundEdges.pop_back();
+        edgesInGraph.remove(nextEdgeId);
+        auto edge = edgeIdCreator.getEdgeFromId(nextEdgeId);
+        updateEdge(edge.first, edge.second);
+        return edge;
+    }
+
+    bool hasNextEdge() {
+        return edgesInGraph.size() > 0;
+    }
+
+protected:
+    void getNextRoundEdges() {
+        EDGEID_T numUpdates = 0;
+        for(EDGEID_T edgeId : edgesInGraph) {
+            pair<NODE_T, NODE_T> edge = edgeIdCreator.getEdgeFromId(edgeId);
+            NODE_T u = edge.first;
+            NODE_T v = edge.second;
+            assert(g.getEdgeRank(u, v) == EDGERANK_INFINIY);
+            g.setEdgeRank(u, v, EDGERANK_INFINIY - 1);
+            auto shortestPathsLost = getShortestPathsLost<false>(edge.first, edge.second, g.getEdgeWeight(u, v), g, query);
+            g.setEdgeRank(u, v, EDGERANK_INFINIY);
+            numShortcutEdges[edgeId] = mvc.getMinimumVertexCoverSize(shortestPathsLost.first);
+            currentRoundEdges.push_back(edgeId);
+        }
+
+        std::sort(currentRoundEdges.begin(), currentRoundEdges.end(), [&] (EDGEID_T i, EDGEID_T j) {
+                return numShortcutEdges[i] < numShortcutEdges[j];
+            });
+
+        std::cout << "Queued up " << currentRoundEdges.size() << " edges with " << numShortcutEdges[currentRoundEdges.front()] << " to " << numShortcutEdges[currentRoundEdges.back()] << " shortcuts" << std::endl;
+
+    }
+
+    EdgeHierarchyGraph &g;
+    EdgeHierarchyQuery query;
+    EdgeIdCreator edgeIdCreator;
+    BipartiteMinimumVertexCover mvc;
+    vector<EDGEID_T> numShortcutEdges;
+    ArraySet<EDGEID_T> edgesInGraph;
+    vector<EDGEID_T> currentRoundEdges;
+};
